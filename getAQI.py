da62b4aea5dfa83eb6d64e5b914a0550cc140110
young <young.mwy@gmail.com>
2015-05-02 12:45:34 +0800

    update getAQI.py

--- getAQI.py
+++ getAQI.py
@@ -10,24 +10,32 @@ city = 'shanghai'
 token = '5j1znBVAsnSf5xQyNQyq'
 AQI_host = apiAQI + '?city=' + city + '&token=' + token
 
-SH_stations = ('putuo', 'changning', 'hongkou', 'xuhui', 'yangpu', 'qingpu', 'jingan', 'chuansha', 'pudong', 'zhangjiang', 'shanghai')
-
 def getAQI():
     urlfile = urllib2.urlopen(AQI_host)
     html = urlfile.read()
     AQIs = json.loads(html)
+    stations_name = []
+    stations_code = []
+    index = []
+    quality = []
     AQI_dict = {}
     for i in range(len(AQIs)):
-        AQI_dict[SH_stations[i]] = AQIs[i]['aqi']
+        stations_code.append(AQIs[i]['station_code'])
+        stations_name.append(AQIs[i]['position_name'])
+        index.append(AQIs[i]['aqi'])
+        quality.append(AQIs[i]['quality'])
+        if (stations_name[i] != None):
+            AQI_dict[stations_code[i]] = (stations_name[i], index[i], quality[i])
     return AQI_dict
 
 def main():
     AQIs = getAQI()
-    print("上海: " + str(AQIs['shanghai']))
-    print("浦东: " + str(AQIs['pudong']))
-    print("杨浦: " + str(AQIs['yangpu']))
-    print("虹口: " + str(AQIs['hongkou']))
-    print("徐汇: " + str(AQIs['xuhui']))
+    AQIlist = AQIs.items()
+    assert type(AQIs) == dict
+    assert type(AQIlist) == list
+    for i in range(len(AQIlist)):
+        print("%s %3d(%s)  %s" % (AQIlist[i][0], AQIlist[i][1][1], AQIlist[i][1][2], AQIlist[i][1][0]))
+    print("浦东: %d" % AQIs['1149A'][1])
 
 if __name__ == "__main__":
     main()
