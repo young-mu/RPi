815d6dcca0f7eda72c15e934bce6496a723f3454
young <young.mwy@gmail.com>
2015-05-14 22:17:15 +0800

    change 3hours forecast to daily forecast

--- Tools/getWeather.py
+++ Tools/getWeather.py
@@ -3,10 +3,12 @@
 
 import json
 import urllib.request
+import socket
 
+timeout = 5
 kelvin = 273.15
 apiWeather = "http://api.openweathermap.org/data/2.5/weather"
-apiForecast = "http://api.openweathermap.org/data/2.5/forecast"
+apiForecast = "http://api.openweathermap.org/data/2.5/forecast/daily"
 appid = "c9a0753615c01095701a37f97ae76583"
 
 def getMostEle(mylist):
@@ -14,9 +16,10 @@ def getMostEle(mylist):
     mylist_most_count = max(mylist_with_count)
     return mylist_most_count[1]
 
-def getWeatherInner(city):
+def getWeather(city):
     try:
         weather_host = apiWeather + "?q=" + city + "&appid=" + appid
+        socket.setdefaulttimeout(timeout)
         urlfile = urllib.request.urlopen(weather_host)
         html = urlfile.read()
         html = html.decode('utf8', 'strict')
@@ -27,62 +30,41 @@ def getWeatherInner(city):
         ret_dict = {'desc':desc, 'temp':temp, 'humidity':humidity}
         return ret_dict
     except Exception as err:
+        print(err)
         return False
 
-def getWeather(city):
-    while True:
-        ret = getWeatherInner(city)
-        if ret:
-            return ret
-
-def getForecastInner(city):
+def getForecast(city):
     try:
         forecast_host = apiForecast + "?q=" + city + "&appid=" + appid
+        socket.setdefaulttimeout(timeout)
         urlfile = urllib.request.urlopen(forecast_host)
         html = urlfile.read()
         html = html.decode('utf8', 'strict')
         forecast_info = json.loads(html)
         forecast_list = forecast_info['list']
-        forecast_detail = []
-        for i in range(len(forecast_list)):
-            time = forecast_list[i]['dt_txt']
-            desc = forecast_list[i]['weather'][0]['main']
-            temp = round((forecast_list[i]['main']['temp'] - kelvin), 2)
-            item = (time, desc, temp)
-            forecast_detail.append(item)
-        # merge into 4 days (everyday has 8 timepoints)
         forecasts = []
-        for i in range(4):
-            alldescs = []
-            alltemps = []
-            for j in range(8):
-                alldescs.append(forecast_detail[i*8+j][1])
-                alltemps.append(forecast_detail[i*8+j][2])
-            descs = getMostEle(alldescs)
-            mintemp = min(alltemps)
-            maxtemp = max(alltemps)
-            avetemp = round(sum(alltemps)/len(alltemps), 2)
-            oneday = (descs, mintemp, maxtemp, avetemp)
-            forecasts.append(oneday)
+        for i in range(len(forecast_list)):
+            desc = forecast_list[i]['weather'][0]['main']
+            mintemp = round((forecast_list[i]['temp']['min'] - kelvin), 2)
+            maxtemp = round((forecast_list[i]['temp']['max'] - kelvin), 2)
+            item = (desc, mintemp, maxtemp)
+            forecasts.append(item)
         return forecasts
     except Exception as err:
+        print(err)
         return False
-
-def getForecast(city):
-    while True:
-        ret = getForecastInner(city)
-        if ret:
-            return ret
 
 def main():
     weather = getWeather('shanghai')
-    print("天气:%s 温度:%.2f 湿度:%d" % (weather['desc'], weather['temp'], weather['humidity']))
+    if weather is not False:
+        print("天气:%s 温度:%.2f 湿度:%d" % (weather['desc'], weather['temp'], weather['humidity']))
     forecasts = getForecast('shanghai')
-    for i in range(len(forecasts)):
-        desc = forecasts[i][0]
-        mintemp = int(round(forecasts[i][1]))
-        maxtemp = int(round(forecasts[i][2]))
-        print("(第" + str(i+1) + "天) " + "天气:%-8s 温度:%d-%d" % (desc, mintemp, maxtemp))
+    if forecasts is not False:
+        for i in range(len(forecasts)):
+            desc = forecasts[i][0]
+            mintemp = int(round(forecasts[i][1]))
+            maxtemp = int(round(forecasts[i][2]))
+            print("(第" + str(i+1) + "天) " + "天气:%-8s 温度:%d-%d" % (desc, mintemp, maxtemp))
 
 if __name__ == "__main__":
     main()
