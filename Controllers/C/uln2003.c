e370c94c054b228c35e71229ae03b63676a00bf0
young <young.mwy@gmail.com>
2015-05-27 11:06:56 +0800

    add C partial implementation of uln2003, which avoids time inteval between speed switch

---
+++ Controllers/C/uln2003.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <wiringPi.h>
+
+#define handle_error(fmt, args...)  \
+    do {                            \
+        char buf[20];               \
+        sprintf(buf, fmt, ##args);  \
+        perror(buf);                \
+        exit(EXIT_FAILURE);         \
+    } while (0)
+
+int delay[5] = {20, 15, 10, 5, 2};
+
+int main(int argc, const char *argv[])
+{
+    const char *file_dir = "./dir.conf";
+    const char *file_spd = "./spd.conf";
+
+    int fd_dir, fd_spd;
+    int dir, spd;
+    int page_sz = getpagesize();
+
+    fd_dir = open(file_dir, O_RDONLY);
+    if (fd_dir == -1) {
+        handle_error("open %s", file_dir);
+    }
+
+    fd_spd = open(file_spd, O_RDONLY);
+    if (fd_spd == -1) {
+        handle_error("open %s", file_spd);
+    }
+
+    char *mem_dir = mmap(NULL, page_sz, PROT_READ, MAP_PRIVATE, fd_dir, 0);
+    char *mem_spd = mmap(NULL, page_sz, PROT_READ, MAP_PRIVATE, fd_spd, 0);
+
+    while (1) {
+        sleep(2);
+
+        dir = atoi(mem_dir);
+        if (dir !=0 && dir != 1) {
+            fprintf(stderr, "invalid dir value: %d", dir);
+            exit(1);
+        }
+        printf("mem_dir: %d\n", dir);
+
+        spd = atoi(mem_spd);
+        if (spd < 1 || spd > 5) {
+            fprintf(stderr, "invalid spd value: %d", spd);
+            exit(1);
+        }
+        printf("mem_spd: %d\n", spd);
+        printf("delay : %d\n", delay[spd - 1]);
+    }
+
+    close(fd_dir);
+    close(fd_spd);
+    munmap(mem_dir, page_sz);
+    munmap(mem_spd, page_sz);
+
+    return 0;
+}
