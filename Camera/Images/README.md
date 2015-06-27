d470b7f14b291b60cacd02e1854801ca22b4b56f
young <young.mwy@gmail.com>
2015-06-27 11:34:39 +0800

    change camera.php to align with Android camera APP. NOTE: after this CI, maybe HTML5 web APP may not work

---
+++ Camera/Images/README.md
@@ -0,0 +1,1 @@
+This directory is used for holding Images captured by Camera.
--- Web/php/camera.php
+++ Web/php/camera.php
@@ -13,7 +13,8 @@ if (isset($_POST['camera'])) {
         $opt_encode = $_POST['encode'];
         $opt_flip = $_POST['flip'];
         $opt_delay = $_POST['delay'];
-        exec("sudo /srv/http/Camera/vc/capture.sh $opt_resolution $opt_quality $opt_encode $opt_flip $opt_delay", $ret, $status);
+        $opt_name = $_POST['name'];
+        exec("sudo /opt/vc/bin/raspistill $opt_resolution $opt_quality $opt_encode $opt_flip $opt_delay $opt_name", $ret, $status);
         echo $status;
         break;
     default:
