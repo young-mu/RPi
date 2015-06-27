f1bd9f3465278997fb76921224acc4dd3a5599ac
young <young.mwy@gmail.com>
2015-06-27 11:53:30 +0800

    change POST to GET for debugging easily

--- Web/php/camera.php
+++ Web/php/camera.php
@@ -1,19 +1,19 @@
 <?php
 
-if (isset($_POST['camera'])) {
-    switch ($_POST['camera'])
+if (isset($_GET['camera'])) {
+    switch ($_GET['camera'])
     {
     case "status":
         exec("/opt/vc/bin/vcgencmd get_camera | awk -F'=' '{print $3}'", $ret, $status);
         echo "$ret[0]";
         break;
     case "capture":
-        $opt_resolution = $_POST['resolution'];
-        $opt_quality = $_POST['quality'];
-        $opt_encode = $_POST['encode'];
-        $opt_flip = $_POST['flip'];
-        $opt_delay = $_POST['delay'];
-        $opt_name = $_POST['name'];
+        $opt_resolution = $_GET['resolution'];
+        $opt_quality = $_GET['quality'];
+        $opt_encode = $_GET['encode'];
+        $opt_flip = $_GET['flip'];
+        $opt_delay = $_GET['delay'];
+        $opt_name = $_GET['name'];
         exec("sudo /opt/vc/bin/raspistill $opt_resolution $opt_quality $opt_encode $opt_flip $opt_delay $opt_name", $ret, $status);
         echo $status;
         break;
